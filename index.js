5bfbed79e1aa7ed3106a40858cf721d266bb248d
Andy <sepshuncontact@gmail.com>
2019-02-19 04:01:13 -0500

    Newer Icons + Basic Script
    Implemented a basic script to output files where they belong, and added the newer icons released in v0.2.0

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-// ------------------------------- //
-// UNICONS SORT | SORTING SCRIPT - //
-// ------------------------------- //
+// ----------------------------- //
+// UNICONS SORT | SORTING SCRIPT //
+// ----------------------------- //
 
 // Require modules
-const fs = require('fs')
+const fs = require('fs-extra')
 
-// Script...
+
+// Get Icon Data (src/icons.json)
+const iconData = JSON.parse(fs.readFileSync('src/icons.json', 'utf8'))
+
+// Loop through each Category
+iconData.categories.forEach(cat => {
+    
+    // Loop through each Icon in Category
+    cat.icons.forEach(icon => {
+        
+        // Get Icon SVG data
+        fs.readFile(`src/icons/${icon.name}.svg`, 'utf8', (err, svg) => {
+            
+            // Ouput Icon SVG to dist category folder
+            fs.outputFile(`dist/${cat.name}/${icon.name}.svg`, svg, err => {
+                if (err) return console.error(err)
+                console.log(`Successfully output ${cat.name}/${icon.name}.svg`)
+            })
+        })
+    })
+})
